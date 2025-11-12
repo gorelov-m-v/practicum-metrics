f267c9a5838417e9e2ab7d00a785f81112fa6f46
Mikhail Gorelov <gorelov.m@it-webdevelopment.com>
2025-11-12 23:32:23 +0300

    Add support for environment variable

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
 )
 
 var (
@@ -15,4 +17,20 @@ func parseFlags() {
 	flag.IntVar(&flagReportInterval, "r", 10, "report interval in seconds")
 	flag.IntVar(&flagPollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
+
+	if envAddr := os.Getenv("ADDRESS"); envAddr != "" {
+		flagRunAddr = envAddr
+	}
+
+	if envReport := os.Getenv("REPORT_INTERVAL"); envReport != "" {
+		if val, err := strconv.Atoi(envReport); err == nil {
+			flagReportInterval = val
+		}
+	}
+
+	if envPoll := os.Getenv("POLL_INTERVAL"); envPoll != "" {
+		if val, err := strconv.Atoi(envPoll); err == nil {
+			flagPollInterval = val
+		}
+	}
 }
