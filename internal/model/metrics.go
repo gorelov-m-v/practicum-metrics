0803aa15af011f0f0cfa55838a06a6d16a463cbc
Mikhail Gorelov <gorelov.m@it-webdevelopment.com>
2025-11-13 23:12:51 +0300

    Add Metrics model for JSON API

---
+++ internal/model/metrics.go
@@ -0,0 +1,9 @@
+package model
+
+// Metrics представляет структуру для обмена метриками между агентом и сервером в формате JSON
+type Metrics struct {
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+}
